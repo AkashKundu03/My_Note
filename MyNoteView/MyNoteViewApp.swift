879ef106e135e67e3e3c164631e46ca86a10e339
Akash Kundu <akashkundu03work@gmail.com>
2025-09-24 02:25:42 +0530

    Initial Commit

---
+++ MyNoteView/MyNoteViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNoteViewApp.swift
+//  MyNoteView
+//
+//  Created by Akash Kundu on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MyNoteViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
